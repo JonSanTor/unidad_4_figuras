8058d69936729ef13feebc033c11d7c34f239e47
JonSanTor <jony10009@gmail.com>
2022-02-11 17:37:16 +0100

    Creacion del metodo mostrarRectangulo

--- src/main/java/Rectangulo.java
+++ src/main/java/Rectangulo.java
@@ -10,4 +10,7 @@ public class Rectangulo {
     public double obtenerPerimetro(){
         return (lado1*2) + (lado2*2);
     }
+    public void mostrarRectangulo(){
+        //Este método enseñara el Rectángulo
+    }
 }
